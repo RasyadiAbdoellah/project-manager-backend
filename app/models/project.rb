1db9f9fa88292699e3cf16f184e59ca59c4c2652
RasyadiAbdoellah <rfabdoellah@gmail.com>
2018-03-06 10:39:16 -0500

    modify virtual to send project_length in seconds

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,15 +3,8 @@ class Project < ApplicationRecord
 
   def project_length
     if start_date
-      seconds = due_date - start_date 
-      {
-        minutes: (seconds / 60).to_i,
-        hours: (seconds / 3600).to_i,
-        days: (seconds / 86400).to_i,
-        weeks: (seconds / 604800).to_i,
-      }
-    else
-      due_date
+      seconds = due_date - start_date
+      seconds.to_i
     end
   end
 end
